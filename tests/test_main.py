7ac6486d87a7976bc2b7d8d7f94680f10a352b0c
Phil Varner <pvarner@element84.com>
2022-03-18 13:18:05 -0400

    build fixes

--- tests/test_main.py
+++ tests/test_main.py
@@ -14,4 +14,4 @@ def runner() -> CliRunner:
 def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(__main__.main)
-    assert result.exit_code == 0
+    assert not result
